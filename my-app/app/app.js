43a5900ace54309a2c423ce427ca445b3c8829a4
brendan-robert-1 <brendan.robert50point@gmail.com>
2021-02-02 16:23:22 -0500

    Update app.js

--- my-app/app/app.js
+++ my-app/app/app.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3080
 let mysql = require('mysql');
-let connection = mysql.createConnection({
-    host:'',
-    user: '',
-    password:'',
-});
+
 connection.changeUser({database : ''}, function(err) {
     if (err) throw err;
   });
 app.get('/', (req, res)=>{
-   
+   let connection = mysql.createConnection({
+        host:'',
+        user: '',
+        password:'',
+    });
     let sql = '';
     connection.query(sql, (error, results, fields)=>{
         if(error){
